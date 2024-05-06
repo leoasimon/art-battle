1572c05a32a530b7672e4e63f0d3a6de23507530
lsimon <leo@lsimon.org>
2024-05-06 15:01:30 -0400

    client: responsive menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,16 @@
+import MobileMenu from "./MobileMenu";
 import Menu from "./Menu";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between px-4 py-6 text-black bg-yellow">
+    <header className="flex items-center justify-between px-4 py-8 text-black bg-yellow">
       <h1>Artworks battle</h1>
-      <Menu />
+      <div className="block md:hidden">
+        <MobileMenu />
+      </div>
+      <div className="hidden md:block">
+        <Menu />
+      </div>
     </header>
   );
 }
